e89bce65b811894ba2292dd652bf4d5c1ec2aee7
proteus236 <proteus236@gmail.com>
2014-11-12 04:01:20 +0000

    model

--- app/models/PenaltyPeriod.php
+++ app/models/PenaltyPeriod.php
@@ -6,4 +6,11 @@ class PenaltyPeriod extends Eloquent{
 	
 	use SoftDeletingTrait;
 	protected $softDelete = true;
+
+	public function scopeGetPenaltyPeriodByDate($query,$date)
+	{
+		$query->where('date_ini','<=',$date)
+			  ->where('date_end','>=',$date);
+		return $query;
+	}
 }
