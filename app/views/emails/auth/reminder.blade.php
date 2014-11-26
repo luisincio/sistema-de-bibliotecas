e8d24a1c306c0f80fa10fb3ab8bb5ae0a349c289
proteus236 <proteus236@gmail.com>
2014-11-26 22:45:00 +0000

    correccion olvide contrasena

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -7,7 +7,7 @@
 		<h2>Renovación de contraseña</h2>
 
 		<div>
-			Para renovar su contraseña, llene el siguiente formulario: <a href="{{ URL::to('password/reset', array($token)) }}">Haga click aquí</a><br/>
+			Para renovar su contraseña, llene el siguiente formulario: <a href="{{ URL::to('password/reset', array($token)) }}" target="_blank">Haga click aquí</a><br/>
 			Este enlace expirará en {{ Config::get('auth.reminder.expire', 60) }} minutos.</br>
 			Ignore este mensaje si usted no realizó esta petición.
 		</div>
